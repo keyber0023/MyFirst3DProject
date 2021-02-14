7cd924632a2b7dc5f435f5ed263d9f829ec755b9
Keyber Salas <salaskeyber@gmail.com>
2021-02-13 19:13:08 -0500

    First commit

---
+++ Assets/Coin/Coin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
+        Coin.counter = coins.Length;
+        Debug.Log("Coin created " + Coin.counter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Coin.counter--;
+            Debug.Log("Coin Picked. " + Coin.counter + " Letf");
+            if (Coin.counter == 0)
+            {
+                Debug.Log("You Win!");
+                GameObject timeManager = GameObject.Find("TimeManager");//Find by name 
+                Destroy(timeManager);
+                GameObject[] fireworks = GameObject.FindGameObjectsWithTag("Fireworks");
+                foreach (GameObject firework in fireworks) {
+                    firework.GetComponent<ParticleSystem>().Play();
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Manager/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 60f;
+    private float countDown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countDown > 0 && Coin.counter > 0)
+        {
+            countDown -= Time.deltaTime;
+            Debug.Log(countDown + " seconds left");
+        }
+        else {
+            if(Coin.counter > 0)
+            {
+                Debug.Log("You Lost");
+                SceneManager.LoadScene("MainScene");
+            }
+        }
+
+    }
+}
